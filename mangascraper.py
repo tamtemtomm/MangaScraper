70bfd6e5d09e8e0c15b22bea7b16aebfe3868e4a
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2023-01-18 23:58:47 +0700

    Manga Scraper 4.0

--- mangascraper.py
+++ mangascraper.py
@@ -3,10 +3,18 @@ import bs4
 import requests
 from PIL import Image
 import time
-
-url = f'https://komikindo.id/ajin-chapter-1/'
+from PyPDF2 import PdfMerger
+import shutil
  
-def scraper(url):
+def scraper():
+    print ('Enter an URL : ')
+    url = input()
+    url = str(url)
+
+    print('Enter a Filename : ')
+    filename = input()
+    filename = str(filename)
+    
     page = 1
     os.makedirs('Manga')
     while True:
@@ -29,19 +37,31 @@ def scraper(url):
         except:
             print('Scraper Selesai')
             break
-        
+    os.makedirs('Manga/PDF')
     for page in os.listdir('Manga'):
-        print(f'Convert Chapter {page} Ke Dalam Bentuk PDF...')
-        images = []
-        im_1 = Image.open(f'Manga/{page}/pic1.png')
-        
-        for file in sorted([int(file.split('.')[0][3:]) for file in os.listdir(f'Manga/{page}')]):
-            path = f'Manga/{page}/pic{file}.png'
-            images.append(Image.open(path).convert('RGB'))
+        if page != 'PDF':    
+            print(f'Convert Chapter {page} Ke Dalam Bentuk PDF...')
+            images = []
+            im_1 = Image.open(f'Manga/{page}/pic1.png')
             
-        im_1.save(f'Manga/{page}.pdf', save_all=True, append_images=images)
-        print(f'PDF Chapter {page} Siap')
+            for file in sorted([int(file.split('.')[0][3:]) for file in os.listdir(f'Manga/{page}')]):
+                try:
+                    path = f'Manga/{page}/pic{file}.png'
+                    images.append(Image.open(path).convert('RGB'))
+                except: pass
+                
+            im_1.save(f'Manga/PDF/{page}.pdf', save_all=True, append_images=images)
+            print(f'PDF Chapter {page} Siap')
+    
+    print ('Menggabungkan PDF...')
+    merger = PdfMerger()
+    for pdf in [f'Manga/PDF/{file}' for file in os.listdir('Manga/PDF')]:
+        merger.append(pdf)
+    merger.write(f'{filename}.pdf')
+    merger.close()
+    
+    shutil.rmtree('Manga')
     print ('Selesai')
 
 if __name__ == '__main__':
-    scraper(url)
+    scraper()
