97e65dc6dc47d23f089463103bf9e0dbfe2ad641
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2023-01-18 22:13:00 +0700

    Scraper 3.0

--- mangascraper.py
+++ mangascraper.py
@@ -2,8 +2,9 @@ import os
 import bs4
 import requests
 from PIL import Image
+import time
 
-url = f'https://komikindo.id/idolxidol-story-chapter-1/'
+url = f'https://komikindo.id/ajin-chapter-1/'
  
 def scraper(url):
     page = 1
@@ -12,12 +13,15 @@ def scraper(url):
         print(f'Import Gambar Chapter {page}...')
         os.makedirs(f'Manga/{page}')
         web = requests.get(url)
+        time.sleep(5)
         images = bs4.BeautifulSoup(web.content, 'lxml').find('div', attrs={'id':'chimg-auh'}).find_all('img')
+        pic_num = 1 
         for image in images:
             img = image['src']
             img_data = requests.get(img).content
             with open(f'Manga/{page}/pic{pic_num}.png', 'wb') as handler:
                 handler.write(img_data)
+            pic_num +=1
         try :    
             url = bs4.BeautifulSoup(web.content, 'lxml').find('a', text='Chapter Selanjutnya »')['href']
             print(f'Chapter {page} Selesai')
